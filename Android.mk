a0649cea4e61351e3467419be0d56b68c889799d
ata-kaner <ata_kaner@outlook.com>
2024-01-16 13:37:46 +0300

    Initial device tree for r8q
    Signed-off-by: ata-kaner <ata.kaner03@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),r8q)
+
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+endif
---
+++ BoardConfig.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/r8q
+
+include device/samsung/sm8250-common/BoardConfigCommon.mk
+
+# Multi-SKU
+TARGET_HAVE_MULTI_SKU := true
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/r8q_eur_openx_defconfig
+BOARD_NAME := SRPUB26A007
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := r8q
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
