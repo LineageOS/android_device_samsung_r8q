b66c18e4bafc9726194f2635f3a5d34dad79b4c5
ata-kaner <ata.kaner03@gmail.com>
2024-04-04 08:36:41 +0300

    r8q: Decommonize r8q from common tree
    Signed-off-by: ata-kaner <ata.kaner03@gmail.com>

--- device.mk
+++ device.mk
@@ -21,9 +21,37 @@ DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 # call the common setup
 $(call inherit-product, device/samsung/sm8250-common/common.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/audio_platform_info_diff.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_diff.xml \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Init files
 PRODUCT_PACKAGES += \
-    init.r8q.rc
+    init.r8q.rc \
+    init.nfc.sh
+
+# NFC
+PRODUCT_PACKAGES += \
+    android.hardware.nfc-service.nxp
+
+# Disabled NFC inits
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/init/android.hardware.nfc-service.nxp.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.nfc-service.nxp.rc \
+    $(DEVICE_PATH)/configs/init/android.hardware.nfc@1.2-service.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.nfc@1.2-service.samsung.rc
+
+# NFC configs
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(DEVICE_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf \
+    $(DEVICE_PATH)/configs/nfc/libnfc-nci-NXP_SN100U.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci-NXP_SN100U.conf \
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.r8q
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Inherit r8q blobs
 $(call inherit-product, vendor/samsung/r8q/r8q-vendor.mk)
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,6 +3,14 @@ LOCAL_PATH := $(call my-dir)
 # Init files
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.nfc.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.nfc.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/bin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.r8q.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
