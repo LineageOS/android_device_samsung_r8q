21b2a1ae08e201a7ae18d412c8b3915395b6a26f
ata-kaner <ata.kaner03@gmail.com>
2024-05-03 08:42:44 +0300

    r8q: Update PRIVATE_BUILD_DESC
    Signed-off-by: ata-kaner <ata.kaner03@gmail.com>

--- lineage_r8q.mk
+++ lineage_r8q.mk
@@ -40,4 +40,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
 # Vendor fingerprint
 BUILD_FINGERPRINT := "samsung/r8qxx/r8q:11/RP1A.200720.012/G780GXXS8EXC1:user/release-keys"
-PRIVATE_BUILD_DESC := "r8qxx-user 13 TP1A.220624.014 G780GXXS8EXC1 release-keys"
+PRIVATE_BUILD_DESC := "r8qxx-user 11 RP1A.200720.012 G780GXXS8EXC1 release-keys"
