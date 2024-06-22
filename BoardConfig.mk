52bc5d10ebb7c77d5e6565b597814334b6f2693c
ata-kaner <ata.kaner03@gmail.com>
2024-06-22 08:38:09 +0300

    r8q: Include r8q config fragment
    Change-Id: Ie50fb3e43e7b7a7d11c6bed54abdb830b3057138

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_HAVE_MULTI_SKU := true
 include device/samsung/sm8250-common/BoardConfigCommon.mk
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/r8q_eur_openx_defconfig
+TARGET_KERNEL_CONFIG += vendor/samsung/r8q.config
 BOARD_NAME := SRPUB26A007
 
 # OTA assert
