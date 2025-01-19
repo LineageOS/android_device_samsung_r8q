9388c775687643cf758663f06f2b5639b44ea58d
Bruno Martins <bgcngm@gmail.com>
2025-01-19 18:08:42 +0000

    r8q: Setup ELF checks
    Change-Id: I38e1b2cf503c1f2cc4aae93709bd49ca5c4d6dd6

--- extract-files.sh
+++ extract-files.sh
@@ -14,6 +14,26 @@ fi
 
 set -e
 
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        libsecril-client | \
+            libwpa_client) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 export DEVICE=r8q
 export DEVICE_COMMON=sm8250-common
 export VENDOR=samsung
