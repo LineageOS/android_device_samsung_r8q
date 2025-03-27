5eb7bd2444242adc23403239bdcddd293bf2f43c
Atakan <ata.kaner03@gmail.com>
2025-03-27 14:54:53 +0300

    r8q: Build Secure Element HAL's from source
    Change-Id: I803e6ff4dd78f2bf3e7de30556c45ffc5008446f

--- device.mk
+++ device.mk
@@ -53,6 +53,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Secure Element
+PRODUCT_PACKAGES += \
+    android.hardware.secure_element-service.nxp \
+    android.hardware.secure_element-service.thales
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.samsung
