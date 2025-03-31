2f176bdc38403eae08db3bdbd57d0c952812ed7d
Bruno Martins <bgcngm@gmail.com>
2025-03-31 09:43:47 +0100

    r8q: Remove lib_fixups_user_type usage
    Change-Id: Ifdb575dcd082591406897a1163b224c861a6a4e8

--- extract-files.py
+++ extract-files.py
@@ -10,7 +10,6 @@ from extract_utils.fixups_blob import (
 )
 from extract_utils.fixups_lib import (
     lib_fixups,
-    lib_fixups_user_type,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -27,10 +26,6 @@ blob_fixups: blob_fixups_user_type = {
         .sig_replace('A0 06 40 F9 6D 66 00 94 80 01 00 34 A2 EA FF D0 43 EC FF 90 65 EB FF F0 E6 03 00 2A', '1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5')
         .sig_replace('42 50 3D 91 63 3C 13 91 A5 74 31 91 E4 0E 80 52 E0 03 1F 2A 21 00 80 52 65 D8 FF 97 A0 06 40 F9', '1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 A0 06 40 F9'),
 }  # fmt: skip
-
-lib_fixups: lib_fixups_user_type = {
-    **lib_fixups,
-}
 
 module = ExtractUtilsModule(
     'r8q',
