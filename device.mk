5925ce059aee5a275ec0b2c97acd1ec181519b2e
Atakan <ata.kaner03@gmail.com>
2025-12-11 06:12:43 +0300

    r8q: Properly override SKU specific services
    Be sure that SKU rc is loaded and parsed before the original service entries
    
    Change-Id: I46d4f34c1c85f2bb1169ea81057a724a8796fee1

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_COPY_FILES += \
 
 # Init
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.multi.rc \
+    android.hardware.multi-sku.rc \
     init.r8q.rc
 
 $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_samsung_r8q)
