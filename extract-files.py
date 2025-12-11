3c446ea8ecdac3b2d5a18a16e3b275bb904d574b
Atakan <ata.kaner03@gmail.com>
2025-12-11 05:55:54 +0300

    Revert "r8q: Shim nfc_nci_nxpsn.so and ese_spi_nxp.so with libbase_shim"
    Reason: AIDL SEC NFC is finally ready.
    
    This reverts commit 2d50800469319c2575f3274f0853a0ee81ae8fca.
    
    Change-Id: I819bb2d49a879ba0a6bf52ab54d80940de7dc529

--- extract-files.py
+++ extract-files.py
@@ -25,8 +25,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/hw/com.qti.chi.override.so': blob_fixup()
         .sig_replace('A0 06 40 F9 6D 66 00 94 80 01 00 34 A2 EA FF D0 43 EC FF 90 65 EB FF F0 E6 03 00 2A', '1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5')
         .sig_replace('42 50 3D 91 63 3C 13 91 A5 74 31 91 E4 0E 80 52 E0 03 1F 2A 21 00 80 52 65 D8 FF 97 A0 06 40 F9', '1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 1F 20 03 D5 A0 06 40 F9'),
-    ('vendor/lib64/nfc_nci_nxpsn.so', 'vendor/lib64/ese_spi_nxp.so'): blob_fixup()
-        .add_needed('libbase_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
