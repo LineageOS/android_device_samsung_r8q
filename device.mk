5a3da6232f49b468928374b00d4b1764d3d9a8a2
Atakan <ata.kaner03@gmail.com>
2025-12-30 06:55:10 +0300

    [DNM] r8q: SamsungBiometrics testing
    Change-Id: Id381f1493c9720f88b3e824e28fdc22a7093df7d

--- device.mk
+++ device.mk
@@ -79,6 +79,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # UDFPS
+$(call soong_config_set,qtidisplay,samsung_udfps,true)
+$(call soong_config_set,samsungUdfpsVars,dim_layer_zorder,0x20000001u)
 $(call soong_config_set,samsungUdfpsVars,udfps_zorder,0x20000000u)
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
 
